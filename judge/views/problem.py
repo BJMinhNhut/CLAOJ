ef0930a42ed51441c6bf1df3b51b4460fe016d18
bachtam2001 <bachtam22082001@gmail.com>
2021-10-29 14:46:03 +0700

    recover editorial title

--- judge/views/problem.py
+++ judge/views/problem.py
@@ -294,7 +294,7 @@ class ProblemList(QueryStringSortMixin, TitleMixin, SolvedProblemMixin, ListView
     manual_sort = frozenset(('name', 'group', 'solved', 'type'))
     all_sorts = sql_sort | manual_sort
     default_desc = frozenset(('points', 'ac_rate', 'user_count'))
-    default_sort = 'name'
+    default_sort = 'points'
 
     def get_paginator(self, queryset, per_page, orphans=0,
                       allow_empty_first_page=True, **kwargs):
