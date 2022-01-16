eeed2195b21f94a1e71c50e61a5d7b5c9742ab7a
bachtam2001 <bachtam22082001@gmail.com>
2022-01-16 11:12:25 +0700

    raise DATA_UPLOAD_MAX_NUMBER_FIELD

--- dmoj/settings.py
+++ dmoj/settings.py
@@ -148,7 +148,9 @@ JQUERY_JS = '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'
 FONTAWESOME_CSS = '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'
 DMOJ_CANONICAL = ''
 
-TESTCASE_VISIBLE_LENGTH = 70
+TESTCASE_VISIBLE_LENGTH = 64
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 3500
 
 # Application definition
 
