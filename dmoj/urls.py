812e30b8be07d789ef5a64adbb8211a32ebaaeb1
bachtam2001 <bachtam22082001@gmail.com>
2022-06-18 16:04:02 +0700

    Remove Moss Folder

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -306,6 +306,8 @@ urlpatterns = [
             url(r'^edit$', blog.BlogPostEdit.as_view(), name='blog_post_edit'),
         ])),
         url(r'^new$', blog.BlogPostCreate.as_view(), name='blog_post_new'),
+        url('^upvote$', blog.upvote_blog, name='blog_upvote'),
+        url('^downvote$', blog.downvote_blog, name='blog_downvote'),
     ])),
 
     url(r'^license/(?P<key>[-\w.]+)$', license.LicenseDetail.as_view(), name='license'),
